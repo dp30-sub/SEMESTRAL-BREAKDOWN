bad35171e4f0233f95f706ad22df5425b40584fc
dp30 <davepaunil30@gmail.com>
2025-11-12 15:38:03 +0800

    adjusted set value

--- game/action/study.java
+++ game/action/study.java
@@ -8,8 +8,8 @@ public class Study extends Action
     public void perform(Player player)
     {
         player.setGrade(player.getGrade() + 3); // add getter and setter called setGrade and getGrade
-        player.setStress(player.getStress()+ 5 ); //  add getter and setter called setStress and getStress
-        player.setEnergy(player.getEnergy() -10);  // // add getter and setter called setEnergy and getEnergy
+        player.setStress(player.getStress()+ 6 ); //  add getter and setter called setStress and getStress
+        player.setEnergy(player.getEnergy() -12);  // // add getter and setter called setEnergy and getEnergy
         System.out.println("Congratulations, your grades improved but you feel tired");
     }
 }
