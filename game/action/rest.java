48be3f0bfbec574670b9b889344613bf2a9ae0f8
dp30 <davepaunil30@gmail.com>
2025-11-12 15:38:40 +0800

    adjust rest value

--- game/action/rest.java
+++ game/action/rest.java
@@ -5,7 +5,7 @@ public class Rest extends Action {
 
     @Override
     public void perform(Player player) {
-        player.setEnergy(player.getEnergy() + 15); 
+        player.setEnergy(player.getEnergy() + 25); 
         player.setStress(player.getStress() - 10); 
         System.out.println("You took a good rest and feel refreshed!");
     }
